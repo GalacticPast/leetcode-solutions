e22333f7a02544815309edf7a1c9080f5ae816ff
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2026-08-05 19:11:46 -0400

    day 31

--- 48/main.cpp
+++ 48/main.cpp
@@ -14,6 +14,7 @@ using std::pair;
 using std::sort;
 using std::stack;
 using std::string;
+using std::swap;
 using std::unordered_map;
 using std::unordered_set;
 using std::vector;
@@ -21,40 +22,22 @@ using std::vector;
 class Solution
 {
   public:
-    void mat_multiply(vector<vector<int>> &matrix)
-    {
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        vector<vector<int>> rev_iden(m, vector<int>(n, 0));
-        vector<vector<int>> iden(m, vector<int>(n, 0));
-        for (int i = 0; i < m; i++)
-        {
-            rev_iden[i][n - i - 1] = 1;
-            iden[i][i]             = 1;
-        }
-        for (int i = 0; i < m; i++)
-        {
-            int ind = 0;
-            for (int j = 0; j < n; j++)
-            {
-                matrix[i][j] = matrix[i][j] * rev_iden[ind][n - 1 - ind];
-                ind++;
-            }
-        }
-        for (int i = 0; i < m; i++)
-        {
-            int ind = 0;
-            for (int j = 0; j < n; j++)
-            {
-                matrix[i][j] = matrix[i][j] * iden[ind][ind];
-                ind++;
-            }
-        }
-    }
     void rotate(vector<vector<int>> &matrix)
     {
-        mat_multiply(matrix);
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
 
